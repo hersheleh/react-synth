7d91c84352a9e4b4a15f0a0193c59613ddca8e8b
Grisha <hersheleh@gmail.com>
2023-12-08 20:26:04 -0800

    made the knob rotate with scroll wheel

--- app/temp/page.tsx
+++ app/temp/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import type { WheelEvent } from "react";
+import { useState } from 'react';
 
 export default function Square() {
   return (
@@ -9,10 +13,29 @@ export default function Square() {
 }
 
 function Knob() {
+
+  const [rotation, setRotation] = useState(0);
+  const [transform, setTransform] = useState(
+    `rotateZ(${rotation}) scale(50%, 10%) translate(50%)`);
+
+  function moveDial(event: WheelEvent) {
+    // console.log(`ScrollWheel ${event.deltaX} ${event.deltaY} ${event.deltaZ}`);
+    let newRotation = Math.abs((rotation + (event.deltaY-115)) % 360)
+    setRotation(newRotation);
+    console.log(newRotation);
+    // setTransform(`rotateZ(${newRotation}) scale(50%, 10%) translate(50%)`);
+    // console.log(`Transform: ${transform}`)
+  }
+
   return (
     <span className="knob-outer">
-      <span className="knob-inner">
-        <span className="dial">
+      <span
+        className="knob-inner"
+        onWheel={moveDial}>
+        <span
+          style={{ transform:  `rotateZ(${rotation}deg) scale(50%, 10%) translate(50%)`}}
+          // onMouseDown={moveDial}
+          className="dial">
         </span>
       </span>
     </span>
